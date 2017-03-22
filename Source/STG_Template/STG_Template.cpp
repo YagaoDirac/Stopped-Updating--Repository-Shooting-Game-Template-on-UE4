d1aa4d012c7f9f78a21a40f4969caa4a551bc952
YagaoDirac <yagaodirac@gmail.com>
2017-03-22 21:13:08 +0800

    totally get rid of cpp

--- Source/STG_Template/STG_Template.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "STG_Template.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, STG_Template, "STG_Template" );
--- Source/STG_Template/STG_TemplateGameModeBase.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "STG_Template.h"
-#include "STG_TemplateGameModeBase.h"
-
-
-
-
